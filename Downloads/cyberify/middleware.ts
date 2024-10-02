9601e63799ec8d733dfd524d406672ebf096deda
Dinakar S <maziyadinakar@gmail.com>
2024-10-02 16:24:14 +0530

    Update middleware.ts

--- Downloads/cyberify/middleware.ts
+++ Downloads/cyberify/middleware.ts
@@ -1,5 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
 const publicRoutes = [
     '/',
     '/events/:id',
